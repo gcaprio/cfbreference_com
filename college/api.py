bf71212517170b06e14e5e5bb5b6aa72815cb9d5
Derek Willis <dwillis@gmail.com>
2012-08-05 21:05:47 -0400

    added team names to game api response

--- college/api.py
+++ college/api.py
@@ -11,17 +11,6 @@ class CollegeResource(ModelResource):
         resource_name = 'college'
         excludes = ['updated', 'id']
         allowed_methods = ['get']
-
-class GameResource(ModelResource):
-    
-    class Meta:
-        queryset = Game.objects.filter(has_stats=True).order_by('-date')
-        resource_name = 'game'
-        excludes = ['id']
-        allowed_methods = ['get']
-    
-    def dehydrate_ncaa_xml(self, bundle):
-        return bundle.data['ncaa_xml'].strip()
         
 
 class CollegeYearResource(ModelResource):
@@ -53,4 +42,20 @@ class CollegeCoachResource(ModelResource):
         resource_name = 'college_coach'
         excludes = ['updated', 'id']
         allowed_methods = ['get']
-        
+
+class GameResource(ModelResource):
+
+    team1 = fields.ToOneField(CollegeYearResource, 'team1')
+    team2 = fields.ToOneField(CollegeYearResource, 'team2')
+    
+    team1_name = fields.CharField(attribute='team1_name')
+    team2_name = fields.CharField(attribute='team2_name')
+
+    class Meta:
+        queryset = Game.objects.filter(has_stats=True).order_by('-date')
+        resource_name = 'game'
+        excludes = ['id']
+        allowed_methods = ['get']
+
+    def dehydrate_ncaa_xml(self, bundle):
+        return bundle.data['ncaa_xml'].strip()
